a7e56b5c81010e586e778eff933006c00946c41d
kma <mazouzi@gmail.com>
2017-05-03 15:03:44 +0200

    Delete build-local.sh

--- build-local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Building local image..."
-
-sudo singularity create -s 1000 mycontainer.img
-
-echo "Bootstraping image..."
-sudo singularity bootstrap Singularity mycontainer.img
